fc95f3dc0ff39c64905f93c3ba5e93a209d3e211
nlj <nlj1999@hotmail.com>
2017-08-11 20:54:01 +0800

    The first time trying Django

---
+++ article/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+def home(request):
+	post_list = Article.objects.all()
+	return render(request, 'home.html', {'post_list': post_list})
+
+
